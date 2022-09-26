ab9ec55245331aaec01c598971fb30c0be7cdad0
Efrain Quiñonez Lopez <efrainlp1995@gmail.com>
2022-09-25 21:43:43 -0500

    graphql support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import HStoreApp from "./HStoreApp";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://3.91.149.21:3001/graphql/",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HStoreApp />
+  <ApolloProvider client={client}>
+    <HStoreApp />
+  </ApolloProvider>
 );
